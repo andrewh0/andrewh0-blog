7f3683ffe3f4491402f2a899f121091fe6f05d71
Andrew H <andrew.leean.ho@gmail.com>
2022-12-16 22:48:49 -0800

    Update copy

--- src/components/projectTitle.tsx
+++ src/components/projectTitle.tsx
@@ -1,6 +1,17 @@
 import { Themed, Box, useThemeUI } from "theme-ui";
+import { keyframes } from "@emotion/react";
 
-const ProjectTitle = ({ name, year }: { name: string; year: string }) => {
+const fadeIn = keyframes({ from: { opacity: 0.25 }, to: { opacity: 1 } });
+
+const ProjectTitle = ({
+  name,
+  year,
+  isActive = false,
+}: {
+  name: string;
+  year: string;
+  isActive?: boolean;
+}) => {
   const { theme }: { theme: any } = useThemeUI();
 
   return (
@@ -12,7 +23,24 @@ const ProjectTitle = ({ name, year }: { name: string; year: string }) => {
         justifyContent: "space-between",
       }}
     >
-      <Themed.h2 sx={{ m: 0 }}>{name}</Themed.h2>
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+        <Themed.h2 sx={{ my: 0, mr: "16px" }}>{name}</Themed.h2>
+        {isActive && (
+          <Box
+            title="Actively developed"
+            sx={{
+              display: "inline-block",
+              bg: "orange9",
+              color: "white",
+              borderRadius: "100%",
+              fontSize: 0,
+              width: "8px",
+              height: "8px",
+              animation: `${fadeIn} 2s ease-out infinite alternate`,
+            }}
+          />
+        )}
+      </Box>
       <span sx={{ fontFamily: "monospace", color: "gray11", fontSize: [1, 2] }}>
         {year}
       </span>
