a613be5a3959b0d12bc8e2bdc0b273c8f771c2fa
Andrew H <andrew.leean.ho@gmail.com>
2022-12-25 16:12:49 -0800

    Fix project page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,7 +1,8 @@
-import { Box } from "theme-ui";
+import { Box, Themed } from "theme-ui";
 import Head from "next/head";
 import Layout from "components/layout";
 import ProjectMarkdown from "markdown/projects.mdx";
+import SubpageNavigation from "components/subpageNavigation";
 
 const Projects = () => {
   return (
@@ -10,6 +11,8 @@ const Projects = () => {
         <title>Projects &middot; Andrew Ho</title>
       </Head>
       <Box>
+        <SubpageNavigation previousPagePath="/" previousPageLabel="Home" />
+        <Themed.h1 sx={{ mb: 5 }}>Projects</Themed.h1>
         <ProjectMarkdown />
       </Box>
     </Layout>
