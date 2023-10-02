8a837e15e4695e34c0918621c70a7af4b28b936f
Andrew H <andrew.leean.ho@gmail.com>
2023-10-01 22:46:41 -0700

    Use radix colors, add typography

--- src/components/postBody.tsx
+++ src/components/postBody.tsx
@@ -1,19 +1,13 @@
 import MDX from "@mdx-js/runtime";
-import { useThemedStylesWithMdx } from "@theme-ui/mdx";
-import { Box } from "theme-ui";
 
 type Props = {
   content: string;
 };
 
-const PostBody = ({ content }: Props) => {
-  const componentsWithStyles = useThemedStylesWithMdx(null);
-  return (
-    // This allows margins to collapse. Otherwise the parent is display: flex.
-    <Box sx={{ display: "block" }}>
-      <MDX components={componentsWithStyles}>{content}</MDX>
-    </Box>
-  );
-};
+const PostBody = ({ content }: Props) => (
+  <div className="prose dark:prose-invert">
+    <MDX>{content}</MDX>
+  </div>
+);
 
 export default PostBody;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      blue: {
+        1: "var(--blue-1)",
+        2: "var(--blue-2)",
+        3: "var(--blue-3)",
+        4: "var(--blue-4)",
+        5: "var(--blue-5)",
+        6: "var(--blue-6)",
+        7: "var(--blue-7)",
+        8: "var(--blue-8)",
+      },
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
