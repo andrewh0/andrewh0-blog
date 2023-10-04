fc15c69dcf2888a5821804329f0ea5c7d1f3787b
Andrew H <andrew.leean.ho@gmail.com>
2023-10-03 21:38:44 -0700

    Remove unused css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,6 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { AppProps } from "next/app";
 
 import theme from "components/theme";
-import "../styles/reset.css";
-// import "../styles/normalize.css";
 import "../styles/global.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
