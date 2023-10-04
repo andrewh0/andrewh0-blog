6c8e94724972b702bf1876d9e4d33c333d557a8c
Andrew H <andrew.leean.ho@gmail.com>
2023-10-03 21:18:11 -0700

    Do not use Themed.p

---
+++ src/components/content/text.tsx
@@ -0,0 +1,5 @@
+const Text = ({ ...props }) => {
+  return <p className="mb-4 font-sans text-gray-12" {...props} />;
+};
+
+export default Text;
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,6 +7,7 @@ import dogAndMe from "../../public/andrew-dog.jpg";
 import Image from "components/content/image";
 import Heading from "components/content/heading";
 import Link from "components/content/link";
+import Text from "components/content/text";
 
 const About = () => (
   <Layout>
@@ -17,35 +18,35 @@ const About = () => (
       <SubpageNavigation previousPagePath="/" previousPageLabel="Home" />
       <Heading title="About" />
       <Image src={dogAndMe} alt="My dog and me" />
-      <Themed.p>
+      <Text>
         Hi, I&apos;m Andrew. I&apos;m a software engineer based in the San
         Francisco Bay Area.
-      </Themed.p>
-      <Themed.p>
+      </Text>
+      <Text>
         Right now, I&apos;m at{" "}
         <Link href="https://www.nexhealth.com">NexHealth</Link> building
         software to connect patients, doctors, and developers. Before, I was at{" "}
         <Link href="https://landed.com">Landed</Link>,{" "}
         <Link href="https://fig.co">Fig</Link>, and{" "}
         <Link href="https://ebay.com">eBay</Link>.
-      </Themed.p>
+      </Text>
       <Themed.hr />
-      <Themed.p>
+      <Text>
         I could not have arrived at where I am today without the support of
         other engineers, teachers, mentors, and the programming community, so my
         personal website is dedicated to sharing what I&apos;ve learned so{" "}
         {/* These non-breaking spaces prevent this from looking like an unordered list on mobile. */}
         far&nbsp;— my knowledge, mistakes, and experiences&nbsp;— to help other
         people build something they&apos;re proud of.
-      </Themed.p>
-      <Themed.p>
+      </Text>
+      <Text>
         This website is{" "}
         <Link href="https://github.com/andrewh0/andrewh0-blog">
           open-source
         </Link>{" "}
         and so are many of my <Link href="/projects">projects</Link>. Feel free
         to email me with any questions or reach out elsewhere on the web.
-      </Themed.p>
+      </Text>
     </Box>
   </Layout>
 );
