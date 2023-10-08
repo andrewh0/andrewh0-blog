41617e91f01e9403ed5a850af2bf4a389477f26f
Andrew H <andrew.leean.ho@gmail.com>
2023-10-08 12:32:21 -0700

    Refactor footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,4 @@
 import React, { AnchorHTMLAttributes } from "react";
-import classNames from "classnames";
 import Link, { LinkProps } from "next/link";
 
 type CustomLinkProps = LinkProps &
@@ -35,69 +34,47 @@ const StyledLink = ({
   );
 };
 
-const LinkContainer = ({
-  children,
-  isHidden = false,
-}: {
-  children: React.ReactNode;
-  isHidden?: boolean;
-}) => (
-  <span className={classNames("mb-1", isHidden ? "hidden" : "inline")}>
-    {children}
-  </span>
-);
-
 const Footer = () => (
-  <footer className="mb-4 mt-32 flex flex-col px-2 text-xs">
-    <div className="mb-2 flex flex-wrap space-x-4">
-      <LinkContainer>
-        <StyledLink
-          href="mailto:hello@andrewho.me"
-          isExternal
-          title="hello@andrewho.me"
-        >
-          Email
-        </StyledLink>
-      </LinkContainer>
-      <LinkContainer>
-        <StyledLink
-          href="https://github.com/andrewh0"
-          isExternal
-          title="andrewh0 on GitHub"
-        >
-          GitHub
-        </StyledLink>
-      </LinkContainer>
-      <LinkContainer>
-        <StyledLink
-          href="https://x.com/andrewlho_codes"
-          isExternal
-          title="andrewlho_codes on X"
-        >
-          X
-        </StyledLink>
-      </LinkContainer>
-      <LinkContainer isHidden>
-        <StyledLink
-          rel="me"
-          href="https://mastodon.social/@andrewh0"
-          isExternal
-          title="andrewh0 on Mastodon"
-        >
-          Mastodon
-        </StyledLink>
-      </LinkContainer>
-      <LinkContainer>
-        <StyledLink
-          href="https://linkedin.com/in/andrewh0"
-          isExternal
-          title="andrewh0 on LinkedIn"
-        >
-          LinkedIn
-        </StyledLink>
-      </LinkContainer>
+  <footer className="mb-4 mt-32 flex flex-col gap-y-2 px-2 text-xs">
+    <div className="flex flex-wrap gap-x-4 gap-y-1">
+      <StyledLink
+        href="mailto:hello@andrewho.me"
+        isExternal
+        title="hello@andrewho.me"
+      >
+        Email
+      </StyledLink>
+      <StyledLink
+        href="https://github.com/andrewh0"
+        isExternal
+        title="andrewh0 on GitHub"
+      >
+        GitHub
+      </StyledLink>
+      <StyledLink
+        href="https://x.com/andrewlho_codes"
+        isExternal
+        title="andrewlho_codes on X"
+      >
+        X
+      </StyledLink>
+      <StyledLink
+        rel="me"
+        href="https://mastodon.social/@andrewh0"
+        isExternal
+        title="andrewh0 on Mastodon"
+      >
+        Mastodon
+      </StyledLink>
+      <StyledLink
+        href="https://linkedin.com/in/andrewh0"
+        isExternal
+        title="andrewh0 on LinkedIn"
+      >
+        LinkedIn
+      </StyledLink>
     </div>
-    <div className="flex-start flex flex-col sm:flex-row sm:justify-between">
+    <div className="flex-start flex flex-col gap-y-2 sm:flex-row sm:justify-between">
       <StyledLink href="/pale-blue-dot">
         <i>On a mote of dust suspended in a sunbeam.</i>
       </StyledLink>
