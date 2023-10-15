d4c25d83537205c7bb70e17ee871ae93a557fad0
Andrew H <andrew.leean.ho@gmail.com>
2023-10-15 13:07:29 -0700

    Force dark mode on SF page

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,16 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import { usePathname } from "next/navigation";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+  const pathname = usePathname();
+  return (
+    <ThemeProvider
+      attribute="class"
+      forcedTheme={pathname === "/sf" ? "light" : undefined}
+    >
+      {children}
+    </ThemeProvider>
+  );
 }
