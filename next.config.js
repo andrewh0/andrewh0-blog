334b6a703e5478b42a48a28468a388668c7a6153
Andrew H <andrew.leean.ho@gmail.com>
2023-10-15 09:27:17 -0700

    Update config

--- next.config.js
+++ next.config.js
@@ -5,13 +5,6 @@ const nextConfig = {
   poweredByHeader: false,
   reactStrictMode: true,
   pageExtensions: ["ts", "tsx", "js", "jsx"],
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages (mdx) that depend on `fs` module
-    if (!isServer) {
-      config.resolve.fallback.fs = false;
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig;
